927ead5a432cb8fc82273db26491ea0fec606afb
MustafaK01 <mestefe119@gmail.com>
2023-04-28 00:38:27 +0300

    edit

--- src/main/java/com/lfs/Main.java
+++ src/main/java/com/lfs/Main.java
@@ -1,12 +1,30 @@
 package com.lfs;
+import com.lfs.core.FrameManager;
 import com.lfs.core.WindowManager;
 
 public class Main {
+
+    private static WindowManager window;
+    private static Game game;
+
     public static void main(String[] args) {
-        WindowManager window = new WindowManager("ads",1366,720,false);
-        window.init();
-        while(!window.windowShouldClose()){
-            window.update();
+        window = new WindowManager("",1366,720,false);
+        FrameManager engine = new FrameManager();
+        game = new Game();
+
+        try {
+            engine.start();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        window.clean();
-    }}
+    }
+
+    public static WindowManager getWindow() {
+        return window;
+    }
+
+    public static Game getGame() {
+        return game;
+    }
+
+}
